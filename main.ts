cd9ae261b7c38dd134cf761297569ecf34781208
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 19:10:57 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    while (compteur > 0) {
+        basic.showNumber(compteur)
+        compteur += -1
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+})
+let compteur = 0
+basic.clearScreen()
+compteur = 10
